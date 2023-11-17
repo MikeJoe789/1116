a7ca01c022a1c544b67f1d6ac0b5d73b9090acdd
MikeJoe789 <juntaoli2022@gmail.com>
2023-11-17 11:52:39 +0000

    scss

---
+++ my-project/src/components/Search.jsx
@@ -0,0 +1,12 @@
+import { CiSearch } from "react-icons/ci";
+
+const Search = ()=>{
+    return(
+        <>
+        <CiSearch />
+        <input type="text" placeholder="Search" />
+        </>
+    )
+}
+
+export default Search
